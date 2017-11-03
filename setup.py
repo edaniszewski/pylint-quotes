90ca825698f335afdda728c73167cfdb4419fbd9
Kelvin PAN <kpan@demonware.net>
2017-11-03 10:54:12 +0800

    Bootstrap build process without pylint

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 """Setup for the pylint-quotes package.
 """
 
+import os
+import re
 from setuptools import setup, find_packages
 
-from pylint_quotes import __version__
+
+def find_version(*file_paths):
+    """Return version defined in __init__.py without import pylint"""
+    with open(os.path.join(*file_paths)) as fhandler:
+        version_file = fhandler.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                  version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name='pylint-quotes',
     description='Quote consistency checker for Pylint',
     license='MIT',
-    version=__version__,
+    version=find_version("pylint_quotes", "__init__.py"),
     author='Erick Daniszewski',
     author_email='edaniszewski@gmail.com',
     url='https://github.com/edaniszewski/pylint-quotes',
